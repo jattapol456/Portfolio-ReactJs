265b6b6317ec841149d41721d0c1df32638e299e
jattapol456 <jattapol.aom@gmail.com>
2022-09-26 10:21:35 +0700

    animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect } from "react";
 import Aboutme from "./components/aboutme";
 import Experienc from "./components/experienc";
 import Skill from "./components/skill";
@@ -6,56 +7,61 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 function App() {
-  gsap.registerPlugin(ScrollTrigger);
-  // REVEAL //
-  gsap.utils.toArray(".revealUp").forEach(function (elem) {
-    ScrollTrigger.create({
-      trigger: elem,
-      start: "top 80%",
-      end: "bottom 20%",
-      markers: false,
-      onEnter: function () {
-        gsap.fromTo(
-          elem,
-          { y: 100, autoAlpha: 0 },
-          {
-            duration: 1.25,
-            y: 0,
-            autoAlpha: 1,
-            ease: "back",
-            overwrite: "auto",
-          }
-        );
-      },
-      onLeave: function () {
-        gsap.fromTo(
-          elem,
-          { autoAlpha: 1 },
-          { autoAlpha: 0, overwrite: "auto" }
-        );
-      },
-      onEnterBack: function () {
-        gsap.fromTo(
-          elem,
-          { y: -100, autoAlpha: 0 },
-          {
-            duration: 1.25,
-            y: 0,
-            autoAlpha: 1,
-            ease: "back",
-            overwrite: "auto",
-          }
-        );
-      },
-      onLeaveBack: function () {
-        gsap.fromTo(
-          elem,
-          { autoAlpha: 1 },
-          { autoAlpha: 0, overwrite: "auto" }
-        );
-      },
+  useEffect(() => {
+    animation()
+  }, []);
+
+  const animation = () => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.utils.toArray(".revealUp").forEach(function (elem) {
+      ScrollTrigger.create({
+        trigger: elem,
+        start: "top 80%",
+        end: "bottom 20%",
+        markers: false,
+        onEnter: function () {
+          gsap.fromTo(
+            elem,
+            { y: 100, autoAlpha: 0 },
+            {
+              duration: 1.25,
+              y: 0,
+              autoAlpha: 1,
+              ease: "back",
+              overwrite: "auto",
+            }
+          );
+        },
+        onLeave: function () {
+          gsap.fromTo(
+            elem,
+            { autoAlpha: 1 },
+            { autoAlpha: 0, overwrite: "auto" }
+          );
+        },
+        onEnterBack: function () {
+          gsap.fromTo(
+            elem,
+            { y: -100, autoAlpha: 0 },
+            {
+              duration: 1.25,
+              y: 0,
+              autoAlpha: 1,
+              ease: "back",
+              overwrite: "auto",
+            }
+          );
+        },
+        onLeaveBack: function () {
+          gsap.fromTo(
+            elem,
+            { autoAlpha: 1 },
+            { autoAlpha: 0, overwrite: "auto" }
+          );
+        },
+      });
     });
-  });
+  };
 
   return (
     <main className="lg:px-56 sm:px-24 px-6 bg-gray-200 py-32">
